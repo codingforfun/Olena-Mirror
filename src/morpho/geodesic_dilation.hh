a4d5090c73cc8c5fb028907a3b4427abadcbd2bd
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2002-04-23 15:40:27 +0200

    2002-04-23  Alexandre Duret-Lutz  <duret_g@epita.fr>
    	* NEWS update for 0.4.
    	* src/morpho/reconstruction.hh, src/morpho/geodesic_erosion.hh,
    	src/morpho/geodesic_dilation.hh, src/morpho/watershed.hxx,
    	src/value/color.hh, src/meta/ufuncs.hh, src/meta/ucmp.hh,
    	src/meta/logic.hh, src/meta/control.hh, src/meta/basics.hh,
    	src/core/pred.hh, src/core/internal/window.hh,
    	src/core/internal/w_window.hh, src/core/internal/image3d_data.hh,
    	src/core/internal/image3d.hh, src/core/internal/image2d_data.hh,
    	src/core/internal/image2d.hh, src/core/internal/image1d_data.hh,
    	src/core/internal/image1d.hh, src/convert/conversion_ng_se.hh,
    	src/morpho/thinning.inc, src/morpho/thinning.hh,
    	src/morpho/thickening.inc, src/morpho/thickening.hh,
    	src/level/lut.hh: Update copyright years.
    
        prcs2git-id: 0_3.32

--- src/morpho/geodesic_dilation.hh
+++ src/morpho/geodesic_dilation.hh
@@ -1,5 +1,4 @@
-
-// Copyright 2002  EPITA Research and Development Laboratory
+// Copyright 2001, 2002  EPITA Research and Development Laboratory
 //
 // This file is part of the Olena Library.  This library is free
 // software; you can redistribute it and/or modify it under the terms
