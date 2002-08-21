7a5b2d45d6c7893347306a90fb855bc5cb9e2055
Nicolas Burrus <burrus_n@lrde.epita.fr>
2002-08-21 13:22:38 +0200

    empty
        prcs2git-id: 0_5.16

--- oln/meta/type.hh
+++ oln/meta/type.hh
@@ -1,3 +1,30 @@
+// Copyright 2001, 2002  EPITA Research and Development Laboratory
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330, Boston,
+// MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
 #ifndef OLENA_META_TYPE_HH
 # define OLENA_META_TYPE_HH
 
@@ -30,7 +57,10 @@ namespace type {
   //
   /////////
 
-  struct form
+  // FIXME: form is a namespace to avoid g++-3.2 bug with template function
+  // overloading in class.
+
+  namespace form
   {
     enum {
       _class_                          = 1,
@@ -39,7 +69,7 @@ namespace type {
       //      _template_l_unsigned_class_g_class_    = 4
       // ...
     };
-
+  
     template<unsigned id> struct desc { char _dummy[id]; };
 
     template<class T>
@@ -55,7 +85,7 @@ namespace type {
     // static desc < _template_l_unsigned_class_g_class_ > get();
 
     // ...
-  };
+  }
 
   namespace internal
   {
