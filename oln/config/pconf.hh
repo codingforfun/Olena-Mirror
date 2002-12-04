98f8e2b66d5ea4900669cad572a7c856f3d26b75
Raphaël Poss <raph@lrde.epita.fr>
2002-12-04 16:25:39 +0100

    Index: ChangeLog from  Raphael Poss  <raph@lrde.epita.fr>         * oln/config/pconf.hh: add (useful for initial builds).
        prcs2git-id: 0_5_reconf.3

---
+++ oln/config/pconf.hh
@@ -0,0 +1,10 @@
+#ifndef OLN_PCONF_HH
+# define OLN_PCONF_HH
+
+#define OLN_PACKAGE_BUGREPORT "olena-bugs@lrde.epita.fr"
+#define OLN_PACKAGE_NAME      "olena"
+#define OLN_PACKAGE_STRING    "olena 0.5"
+#define OLN_PACKAGE_TARNAME   "olena"
+#define OLN_PACKAGE_VERSION   "0.5"
+
+#endif
