30dcf278f36a0492c39829900faa912533524fc6
Akim Demaille <akim@lrde.epita.fr>
2003-01-16 18:52:15 +0100

    Index: oln/ChangeLog from   2003-01-16  Akim Demaille  <akim@epita.fr>         * tests/sanity/includes.test: Don't jump over `old/' which no         longer exists.
    	* oln/oln/core/old/image2d_bin.hh, oln/oln/core/old/image2d_bin.hxx
    	* oln/oln/core/old/image2d.hxx, oln/oln/core/old/pred.hh
    	* oln/oln/core/old/rec_window.hh: Remove.
    
        prcs2git-id: 7.16

--- oln/oln/types/optraits_builtins_int.hh
+++ oln/oln/types/optraits_builtins_int.hh
@@ -1,4 +1,4 @@
-// Copyright (C) 2001, 2002  EPITA Research and Development Laboratory
+// Copyright (C) 2001, 2002, 2003  EPITA Research and Development Laboratory
 //
 // This file is part of the Olena Library.  This library is free
 // software; you can redistribute it and/or modify it under the terms
@@ -41,7 +41,7 @@
 //  optraits for builtins
 //
 //  dev note : optraits<builtins> artihmetic operators are only called
-//  when there is an operator_xxx_traits defined, else buitlins are 
+//  when there is an operator_xxx_traits defined, else buitlins are
 //  converted into oln type.
 //
 ///////////////////////////////////////////////////////////////////////////////
