d50597c8da19ac7408cf3e9c670a4c45e449600a
Nicolas Burrus <burrus_n@lrde.epita.fr>
2003-03-12 17:13:14 +0100

    Index: ChangeLog from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    	* config/oln.m4: Add new macros. Simplify flags.
    
    	* configure.ac, Makefile.am, bootstrap.sh: Adapt to m4 changes.
    
    Index: doc/ChangeLog
    from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    
    	* demo/Makefile.am: Remove useless flags.
    
    Index: metalic/ChangeLog
    from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    
    	* tests/check/Makefile.am: Remove useless flags.
    
    Index: olena/ChangeLog
    from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    
    	* tests/check/Makefile.flags, tests/sanity/Makefile.am: Remove
    	useless flags.
    
    	* Makefile.am: Adjust consequently.
    
    	* config/oln.m4, config/oln-local.m4: Move into ../config/.
    
    Index: tools/ChangeLog
    from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    
    	* utilities/Makefile.am: Remove useless flags.
    
    Index: tools/swilena/ChangeLog
    from  Nicolas Burrus  <burrus_n@lrde.epita.fr>
    
    	* python/Makefile.am: Remove useless flags.
    
        prcs2git-id: 8.17

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ sh ./cleanup.sh
 ( cd tools/utilities/morpho && sh ./generate_morpho.sh . )
 ( cd doc/ref && sh ./gen_filelists.sh filelists.make ../../olena/oln )
 autoreconf -v -f -i
-( cd olena/conf && aclocal -I ../config && autoconf -f )
+( cd olena/conf && aclocal -I ../../config && autoconf -f )
 echo
 echo "Reconfiguration done."
