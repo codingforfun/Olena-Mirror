37a1f84d565ee305fd5bfe9c732740ee8e338aab
Sylvain Berlemont <sylvain@lrde.epita.fr>
2003-07-30 16:16:07 +0200

    * tests/topo/test.sh, tests/topo/test-cmap.ref, 	tests/topo/test-inter-pixel.ref: Delete files.
        prcs2git-id: 2003paradigm.65

---
+++ olena/tests/topo/test-dmap.cc
@@ -0,0 +1,36 @@
+#include <ntg/int.hh>
+#include <oln/basics2d.hh>
+#include <oln/topo/dmap.hh>
+
+#include <iostream>
+
+#include "check.hh"
+#include "data.hh"
+
+using namespace oln;
+using namespace ntg;
+using namespace topo;
+
+#define OK_OR_FAIL				\
+      std::cout << "OK" << std::endl;		\
+    else					\
+      {						\
+	std::cout << "FAIL" << std::endl;	\
+	fail = true;				\
+      }
+
+bool
+check()
+{
+  bool fail = false;
+
+  chamfer<int> ch = cityblock();
+
+  image2d<int_u8> src = load(rdata("lena256.pgm"));
+
+  dmap<int_u8, int> d(src.size(), ch);
+
+  d.compute(src);
+
+  return fail;
+}
