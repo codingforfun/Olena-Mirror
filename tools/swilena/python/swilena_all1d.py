732bf5f8d15234f314f2790f302dfa8345d84df0
Nicolas Burrus <burrus_n@lrde.epita.fr>
2003-11-10 14:12:17 +0100

    Index: tools/swilena/ChangeLog from  Nicolas Burrus  <burrus_n@lrde.epita.fr> 	* python/swilena_all1d.py: New. 	* python/swilena_all2d.py: New. 	* python/swilena_all2d.py: New. 	* python/Makefile.am: New.
        prcs2git-id: 10.41

---
+++ tools/swilena/python/swilena_all1d.py
@@ -0,0 +1,14 @@
+# Utility module which includes everything available in swilena
+# FIXME: should be generated
+
+from swilena_image1d import *
+from swilena_imagesize1d import *
+from swilena_point1d import *
+from swilena_morpho1d import *
+from swilena_structelt1d import *
+from swilena_w_win1d import *
+from swilena_conversions1d import *
+from swilena_arith1d import *
+
+from swilena_ntg import *
+from swilena_display import *
