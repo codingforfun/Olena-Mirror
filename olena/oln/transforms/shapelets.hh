73ca2de7eabf12480e472c92c1b484233445fd67
Damien Thivolle <thivol_d@lrde.epita.fr>
2004-06-07 19:41:27 +0200

    Damien Thivolle  <damien@lrde.epita.fr> * oln/transforms/shapelets.hh: Change functions to functors, bug fixes.
        prcs2git-id: 10.189

--- olena/oln/transforms/shapelets.hh
+++ olena/oln/transforms/shapelets.hh
@@ -7,138 +7,159 @@
 #include <oln/core/point2d.hh>
 #include <oln/level/fill.hh>
 #include <vector>
+#include <bits/stl_function.h>
 
 namespace oln
 {
-  inline double
-  hermite(int n, double x)
+  namespace internal
   {
-    assert(n >= 0);
-    if (n == 0)
-      return 1;
-    if (n == 1)
-      return 2. * x;
-    return 2. * (x * hermite(n - 1, x) - (n - 1) * hermite(n - 2, x));
-  }
-
-  inline double
-  fact(double n)
-  {
-    precondition(n >= 0);
-
-    double res = n;
-
-    if (n <= 1)
-      return 1;
-    while (n > 1)
+    template <class T>
+    struct hermite : public std::binary_function<T, int, T>
+    {
+      T operator()(unsigned n, T x)
       {
-	res *= n;
-	n--;
+	if (n == 0)
+	  return 1;
+	if (n == 1)
+	  return 2. * x;
+	return 2. * x * (*this)(n - 1, x) - 2. * double(n - 1) * (*this)(n - 2, x);
       }
-    return res;
-  }
+    };
 
-  inline double
-  beta(int n, double x)
-  {
-    assert(n >= 0);
-    const double c = sqrt(pow(2., n) * sqrt(M_PI) * fact(n));
+    template <class T>
+    struct fact : public std::unary_function<T, T>
+    {
+      T operator() (T n)
+      {
+	precondition(n >= 0);
+	T res = 1;
 
-    assert(finite(hermite(n, x) * exp(x * x / -2.0) / c));
-    return hermite(n, x) * exp(x * x / -2.0) / c;
-  }
+	while (n > 0)
+	  {
+	    res *= n;
+	    n--;
+	  }
+	return res;
+      }
+    };
 
+    template <class T>
+    struct shapelets_beta : public std::binary_function<T, int, T>
+    {
+      T operator()(int n, T x)
+      {
+	assert(n >= 0);
+	const double c = sqrt(pow(2.0, n) * sqrt(M_PI) * T(fact<int>()(n)));
 
+	return hermite<T>()(n, x) * exp(x * x / -2.0) / c;
+      }
+    };
 
-  inline double
-  shapelet_basis_1d(int n, point1d p, double b)
-  {
-    assert(n >= 0 && b >= 0);
-    return beta(n, p.col() / b) / sqrt(b);
-  }
+    template <unsigned Dim, class T>
+    struct shapelets_basis
+    {
+    };
 
-  double
-  shapelet_basis_2d(int m, int n, point2d p, double b)
-  {
-    assert(m >= 0 && n >= 0 && b >= 0);
-    return beta(m, p.col() / b) * beta(n, p.row() / b) / b;
-  }
+    template <class T>
+    struct shapelets_basis<1, T>
+    {
+      T operator() (int n, point1d p, T b)
+      {
+	assert(n >= 0 && b >= 0);
+	return shapelets_beta<T>(n, p.col() / b) / sqrt(b);
+      }
+    };
 
-  //FIXME: ima should be non_vectorial
-  template <class I>
-  std::vector<double>
-  to_shapelet_basis_2d(const oln::abstract::image_with_dim<2, I>& ima,
-		       int m, int n,
-		       double b)
-  {
-    assert(m >= 0 && n >= 0 && b >= 0);
-
-    // Center of the image
-    const int col = ima.ncols() / 2;
-    const int row = ima.nrows() / 2;
-
-    std::vector<double> res(m * n);
-
-    // For all vector in the base of shapelets
-    for (int k = 0; k < m; k++)
-      for (int l = 0; l < n; l++)
-	// res[k * n + l] = scalar product of the image and the function
-	{
-	  double s = 0.0;
-
-	  oln_iter_type(I) it(ima);
-	  for_all(it)
-	    {
-	      s += (ima[it] - ntg_max_val(oln_value_type(I)) / 2) *
-		shapelet_basis_2d(k, l, point2d(it.row() - row, it.col() - col), b);
-	    }
-	  res[k * n + l] = s;
-	}
-    return res;
-  }
+    template <class T>
+    struct shapelets_basis<2, T>
+    {
+      T operator() (int m, int n, T x, T y, T b)
+      {
+	assert(m >= 0 && n >= 0 && b >= 0);
+	return shapelets_beta<T>()(m, x / b) * shapelets_beta<T>()(n, y / b) / b;
+      }
+    };
 
 
-  template <class D>
-  image2d<D>
+    //FIXME: ima should be non_vectorial
+    template <class I>
+    std::vector<double>
+    shapelets(const oln::abstract::image_with_dim<2, I>& ima,
+			 int m, int n,
+			 double b)
+    {
+      assert(m >= 0 && n >= 0 && b >= 0);
+      shapelets_basis<2, double> func;
+
+      // Center of the image
+      const int col = ima.ncols() / 2;
+      const int row = ima.nrows() / 2;
+
+      std::vector<double> res(m * n);
+
+      // For all vector in the base of shapelets
+      for (int k = 0; k < m; k++)
+	for (int l = 0; l < n; l++)
+	  // res[k * n + l] = scalar product of the image and the function
+	  {
+	    double s = 0.0;
+
+	    oln_iter_type(I) it(ima);
+	    for_all(it)
+	      {
+		s += (ima[it] - ntg_max_val(oln_value_type(I)) / 2) *
+		  func(k, l, double(it.row() - row), double(it.col() - col), b);
+	      }
+	    res[k * n + l] = s;
+	  }
+      return res;
+    }
+
+
+    template <class D>
+    image2d<D>
     unshapelets(const std::vector<double> &vec,
 		int m, int n, double b,
 		coord nrows, coord ncols)
-  {
-    precondition(nrows >= 1 && ncols >= 1  &&
-		 m >= 0 && n >= 0 && b >= 0);
+    {
+      precondition(nrows >= 1 && ncols >= 1  &&
+		   m >= 0 && n >= 0 && b >= 0);
 
 
-    typedef image2d<double> tmp_type;
-    tmp_type resf(oln::image2d_size(nrows, ncols, 0));
+      typedef image2d<double> tmp_type;
+      tmp_type resf(oln::image2d_size(nrows, ncols, 0));
 
-    oln_iter_type(tmp_type) it(resf);
+      oln_iter_type(tmp_type) it(resf);
 
-    oln::level::fill(resf, ntg_max_val(D) / 2 );
-    // For each vector
-    for (int k = 0; k < m; k++)
-      for (int l = 0; l < n; l++)
-	// Add the value at the point
-	for_all(it)
-	  resf[it] += vec[k * n + l] *
-	  shapelet_basis_2d(k, l, point2d(it.row() - nrows / 2, it.col() - ncols / 2), b);
+      oln::level::fill(resf, ntg_max_val(D) / 2);
+      // For each vector
+      for (int k = 0; k < m; k++)
+	for (int l = 0; l < n; l++)
+	  // Add the value at the point
+	  for_all(it)
+	    resf[it] += vec[k * n + l] *
+	    shapelets_basis<2, double>()(k, l, double(it.row() - nrows / 2), double(it.col() - ncols / 2), b);
 
-    image2d<D> res(oln::image2d_size(nrows, ncols, 0));
+      image2d<D> res(oln::image2d_size(nrows, ncols, 0));
 
-    for_all(it)
-      {
-	if ( !(resf[it] >= 0 && resf[it] < 256))
-	  {
-	    std::cout << "err:" << resf[it] << std::endl;
-// 	    min = (resf[it] < min)? resf[it]:min;
-// 	    max = (resf[it] >max)? resf[it]:max;
-	    res[it] = resf[it] < 0 ? 0 : ntg_max_val(D);
-	  }
-	else
-	  res[it] = resf[it];
-      }
-    //    std::cout << "min:" << min
-    //	      << " max:" << max << std::endl;
-    return res;
+      for_all(it)
+	{
+	  if ( !(resf[it] >= 0 && resf[it] <= 255))
+	    {
+	      std::cout << "- ";
+	      std::cout << "err:" << resf[it];
+	      // 	    min = (resf[it] < min)? resf[it]:min;
+	      // 	    max = (resf[it] >max)? resf[it]:max;
+	      res[it] = resf[it] < 0 ? 0 : ntg_max_val(D);
+	      std::cout << " -" << std::endl;
+	    }
+	  else
+	    res[it] = resf[it];
+	}
+      //    std::cout << "min:" << min
+      //	      << " max:" << max << std::endl;
+      return res;
+    }
+
   }
-
 }
