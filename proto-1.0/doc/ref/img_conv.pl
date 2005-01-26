a12a44b537343d133d5987ecae70f30f948ef9db
Simon Odou <odou_s@lrde.epita.fr>
2005-01-26 10:43:09 +0000

    2005-01-26  Simon Odou  <simon@lrde.epita.fr>
    	* ref/doxygen.config.in: New. Doxygen configuration file (from
              Olena).
    	* ref/out: New.
    	* ref/out/exdoc.config.in: New. Exdoc configuration (from Olena).
    	* ref/Makefile.am: Use exdoc to generate documentation.
    	* ref/img_conv.pl: New. Image conversion script (from Olena).
    	* ref/exdoc.pl: New. Exdoc to write examples in documentation (from
              Olena).
    	* ref/exdoc.mk.in: New. Makefile for exdoc (from Olena).
    	* vcs: New. Add vcs support for this ChangeLog.
    	* vcs/oln.rb: New. Likewise.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@22 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ proto-1.0/doc/ref/img_conv.pl
@@ -0,0 +1,61 @@
+#! /usr/bin/perl -w
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# 02111-1307  USA
+
+
+use strict;
+use IO::File;
+use Text::Wrap;
+use File::Basename;
+use Getopt::Long qw(GetOptions);
+use Pod::Usage qw(pod2usage);
+
+
+sub conv($ $ $)
+{
+    my $file;
+    my $d = $_[0];
+    my $o = $_[1];
+    my $preserve_name = 0;
+    if ($_[2] =~ /^preserve$/)
+    {
+	$preserve_name = 1;
+    }
+
+    opendir DIR, "$d" or die "Unable to open $d.\n";
+    foreach $file(readdir DIR)
+    {
+	if ($file =~ /(.*)\.(pgm|ppm|ppgm|pppgm|pbm)$/)
+	{
+	    if ($preserve_name == 1)
+	    {
+		print "convert $d/$file to $o/$1_$2.png\n";
+		system "convert $d/$file $o/$1_$2.png";
+	    }
+	    else
+	    {
+		print "convert $d/$file to $o/$1.png\n";
+		system "convert $d/$file $o/$1.png";
+	    }
+	}
+    }
+    closedir DIR or die "unable to close $d.";
+}
+my $argc=@ARGV;
+if ($argc != 3)
+{
+   die "usage: img_conv.pl input-dir output-dir preserve|nopreserve\n";
+}
+conv $ARGV[0], $ARGV[1], $ARGV[2];
