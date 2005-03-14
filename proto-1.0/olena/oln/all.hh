5b3079fd1ce4827958d3ddc44fb0f8c6d82f3305
Damien Thivolle <thivol_d@lrde.epita.fr>
2005-03-14 14:41:43 +0000

    2005-03-14  Damien Thivolle  <damien@lrde.epita.fr>
    	* oln/makefile.src: Add new file.
    	* oln/all.hh: New.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@55 4aad255d-cdde-0310-9447-f3009e2ae8c0

---
+++ proto-1.0/olena/oln/all.hh
@@ -0,0 +1,38 @@
+// Copyright (C) 2005  EPITA Research and Development Laboratory
+//
+// This file is part of the Olena Library.  This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License version 2 as published by the
+// Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; see the file COPYING.  If not, write to
+// the Free Software Foundation, 59 Temple Place - Suite 330, Boston,
+// MA 02111-1307, USA.
+//
+// As a special exception, you may use this file as part of a free
+// software library without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to
+// produce an executable, this file does not by itself cause the
+// resulting executable to be covered by the GNU General Public
+// License.  This exception does not however invalidate any other
+// reasons why the executable file might be covered by the GNU General
+// Public License.
+
+#ifndef OLN_ALL_HH
+# define OLN_ALL_HH
+
+# include <oln/core/1d/image1d.hh>
+# include <oln/core/2d/image2d.hh>
+# include <oln/core/3d/image3d.hh>
+
+# include <oln/io/write_image.hh>
+# include <oln/io/read_image.hh>
+
+#endif // ! OLN_ALL_HH
