cbeb74fd9756a25d7200cf4ccb506674eb80ae1e
Roland Levillain <roland@lrde.epita.fr>
2005-07-08 10:02:00 +0000

    2005-07-08  Roland Levillain  <roland@lrde.epita.fr>
    	Add hit-or-miss opening/closing and hit-or-miss opening/closing of
    	the background.
    
    	* oln/morpho/hit_or_miss.hh: More documentation.
    	(hit_or_miss_opening, hit_or_miss_opening_bg)
    	(hit_or_miss_closing, hit_or_miss_closing_bg): New.
    	* tests/morpho/tests/hit_or_miss_opening,
    	* tests/morpho/tests/hit_or_miss_closing: New tests.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@239 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- proto-1.0/olena/oln/morpho/hit_or_miss.hh
+++ proto-1.0/olena/oln/morpho/hit_or_miss.hh
@@ -32,9 +32,12 @@
 
 # include <oln/utils/record.hh>
 # include <oln/morpho/erosion.hh>
+# include <oln/morpho/dilation.hh>
 # include <oln/level/arith.hh>
 # include <oln/level/invert.hh>
 
+// FIXME: More documentation (in particular, code snippets).  See Olena
+// 0.10's doc.
 
 namespace oln {
 
@@ -83,7 +86,17 @@ namespace oln {
     } // end of namespace oln::morpho::impl
 
 
-    /// Generic hit-or-miss transform (facade). 
+    /*! \brief Preform a hit-or-miss transform (generic facade)
+
+	REF: Soille, 2nd ed., p.140.
+
+        \arg \c I  exact type of the input image.
+	\arg \c W1 exact type of the first structuring element.
+	\arg \c W2 exact type of the second structuring element.
+
+	\param input image to process.
+	\param win1  first structuring element.
+	\param win2  second structuring element.  */
     template<class I, class W1, class W2>
     oln_type_of(I, concrete)
     hit_or_miss(const abstract::image<I>& input,
@@ -98,6 +111,159 @@ namespace oln {
       output = impl::hit_or_miss_(input, win1.exact(), win2.exact());
 
       exiting("morpho::hit_or_miss");
+      return output;
+    }
+
+
+    /*! \brief Perform an hit-or-miss opening.
+
+        Compute the hit-or-miss opening of input by the composite
+	structuring element (win1, win2).
+    
+	REF: Soille, 2nd ed., p.149.
+
+	By definition \a win1 and \a win2 must have the same origin,
+	and need to be disjoint.  This algorithm has been extended to
+	every data types (althought it is not increasing).  Beware the
+	result depends upon the image data type if it is not binary.
+
+	\arg \c I  Exact type of the input image.
+	\arg \c W1 Exact type of the first structuring element.
+	\arg \c W2 Exact type of the second structuring element.
+
+	\param input image to process.
+	\param win1  first structuring element.
+	\param win2  second structuring element.  */
+    template<class I, class W1, class W2>
+    oln_type_of(I, concrete)
+    hit_or_miss_opening(const abstract::image<I>& input,
+			const abstract::window<W1>& win1,
+			const abstract::window<W2>& win2)
+    {
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W1, grid)>::ensure();
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W2, grid)>::ensure();
+      entering("morpho::hit_or_miss_opening");
+
+      oln_type_of(I, concrete) output("output");
+      output = dilation(hit_or_miss(input, win1, win2), -win1);
+
+      exiting("morpho::hit_or_miss_opening");
+      return output;
+    }
+
+    /*! \brief Perform an hit-or-miss opening of background.
+
+        Compute the hit-or-miss opening of the background of input by
+	the composite structuring element (win1, win2).
+
+	REF: Soille, 2nd ed., p.149.
+
+	By definition win1 and win2 must have the same origin, and
+	need to be disjoint.  This algorithm has been extended to
+	every data types (although it is not increasing).  Beware the
+	result depends upon the image data type if it is not bin.
+ 
+	\arg \c I  exact type of the input image.
+	\arg \c W1 exact type of the first structuring element.
+	\arg \c W2 exact type of the second structuring element.
+ 
+	\param input image to process.
+	\param win1  first structuring element.
+	\param win2  second structuring element.  */
+    template<class I, class W1, class W2>
+    oln_type_of(I, concrete)
+    hit_or_miss_opening_bg(const abstract::image<I>& input,
+			   const abstract::window<W1>& win1,
+			   const abstract::window<W2>& win2)
+    {
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W1, grid)>::ensure();
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W2, grid)>::ensure();
+      entering("morpho::hit_or_miss_opening_bg");
+
+      oln_type_of(I, concrete) output("output");
+      output = dilation(hit_or_miss(input, win1, win2), -win2);
+
+      exiting("morpho::hit_or_miss_opening_bg");
+      return output;
+    }
+
+    /*! \brief Perform an hit-or-miss closing.
+
+        Compute the hit-or-miss closing of input by the composite
+	structuring element (win1, win2).  This is the dual
+	transformation of hit-or-miss opening with respect to set
+	complementation.
+    
+	REF: Soille, 2nd ed., p.149.
+
+	By definition \a win1 and \a win2 must have the same origin,
+	and need to be disjoint.  This algorithm has been extended to
+	every data types (althought it is not increasing).  Beware the
+	result depends upon the image data type if it is not binary.
+
+	\arg \c I  Exact type of the input image.
+	\arg \c W1 Exact type of the first structuring element.
+	\arg \c W2 Exact type of the second structuring element.
+
+	\param input image to process.
+	\param win1  first structuring element.
+	\param win2  second structuring element.  */
+    template<class I, class W1, class W2>
+    oln_type_of(I, concrete)
+    hit_or_miss_closing(const abstract::image<I>& input,
+			const abstract::window<W1>& win1,
+			const abstract::window<W2>& win2)
+    {
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W1, grid)>::ensure();
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W2, grid)>::ensure();
+      entering("morpho::hit_or_miss_closing");
+
+      oln_type_of(I, concrete) output("output");
+      // FIXME: Is this correct?  Soille doesn't detail the definition
+      // of the hit-or-miss closing in his book.
+      output = hit_or_miss_opening(level::invert(input), win1, win2);
+
+      exiting("morpho::hit_or_miss_closing");
+      return output;
+    }
+
+    /*! \brief Perform an hit-or-miss closing of the background.
+
+        Compute the hit-or-miss closing of the background of input by
+	the composite structuring element (win1, win2).  This is the
+	dual transformation of hit-or-miss opening of the background
+	with respect to set complementation.
+
+	REF: Soille, 2nd ed., p.149.
+
+	By definition \a win1 and \a win2 must have the same origin,
+	and need to be disjoint.  This algorithm has been extended to
+	every data types (althought it is not increasing).  Beware the
+	result depends upon the image data type if it is not binary.
+
+	\arg \c I  Exact type of the input image.
+	\arg \c W1 Exact type of the first structuring element.
+	\arg \c W2 Exact type of the second structuring element.
+
+	\param input image to process.
+	\param win1  first structuring element.
+	\param win2  second structuring element.  */
+    template<class I, class W1, class W2>
+    oln_type_of(I, concrete)
+    hit_or_miss_closing_bg(const abstract::image<I>& input,
+			const abstract::window<W1>& win1,
+			const abstract::window<W2>& win2)
+    {
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W1, grid)>::ensure();
+      mlc::eq<oln_type_of(I, grid), oln_wn_type_of(W2, grid)>::ensure();
+      entering("morpho::hit_or_miss_closing_bg");
+
+      oln_type_of(I, concrete) output("output");
+      // FIXME: Is this correct?  Soille doesn't detail the definition
+      // of the hit-or-miss closing of the background in his book.
+      output = hit_or_miss_opening_bg(level::invert(input), win1, win2);
+
+      exiting("morpho::hit_or_miss_closing_bg");
       return output;
     }
 
