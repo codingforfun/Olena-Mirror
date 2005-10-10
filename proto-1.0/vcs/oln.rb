08615b2a2c987b455386e0f46a17f93130c2e336
Nicolas Pouillard <ertai@lrde.epita.fr>
2005-10-10 17:14:22 +0000

    Update the Vcs extension to Vcs 0.4.
    - vcs/oln.rb: Add the protocol version and the default commit.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@275 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- proto-1.0/vcs/oln.rb
+++ proto-1.0/vcs/oln.rb
@@ -1,12 +1,17 @@
 class Vcs
+  # See http://rubyforge.org/projects/vcs
+  # and http://vcs.rubyforge.org
 
-  def oln_commit! ( *args )
+  protocol_version '0.1'
+
+  def olena_commit! ( *args )
 
     common_commit!("proto-1.0 <%= rev %>: <%= title %>", *args) do |subject|
       mail!(:to => ['olena-patches@lrde.epita.fr'], :subject => subject)
     end
 
   end
-  alias_command :olnci, :oln_commit
+  alias_command  :olnci, :olena_commit
+  default_commit :olena_commit
 
 end # class Vcs
