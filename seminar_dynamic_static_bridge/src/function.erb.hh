7ccc04a91f44f8b789718a134172be2ac54cff0b
Nicolas Pouillard <ertai@lrde.epita.fr>
2005-11-15 03:36:09 +0000

    Less badly used references.
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@320 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- seminar_dynamic_static_bridge/src/function.erb.hh
+++ seminar_dynamic_static_bridge/src/function.erb.hh
@@ -22,9 +22,9 @@ namespace dyn
   {
 
     generic_fun(const data* obj_ptr,
-                const std::string& kind,
-                const std::string& name,
-                const std::string& header_path="") :
+                const std::string kind,
+                const std::string name,
+                const std::string header_path="") :
       obj_ptr_(obj_ptr), kind_(kind), name_(name), header_path_(header_path) {}
 
     <%- NB_MAX_ARGUMENTS.times do |i| -%>
@@ -46,7 +46,7 @@ namespace dyn
   <% %w[ fun proc ctor method_proc method_fun ctor2 ].each do |name| %>
   struct <%= name %> : public generic_fun
   {
-    <%= name %>(const std::string& name, const std::string& header_path="") :
+    <%= name %>(const std::string name, const std::string header_path="") :
       generic_fun(0, ":<%= name %>", name, header_path) {}
   };
   <% end %>
@@ -54,7 +54,7 @@ namespace dyn
   <% %w[ method_fun2 method_proc2 ].each do |name| %>
   struct <%= name %> : public generic_fun
   {
-    <%= name %>(const data* obj_ptr, const std::string& name, const std::string& header_path="") :
+    <%= name %>(const data* obj_ptr, const std::string name, const std::string header_path="") :
       generic_fun(obj_ptr, ":<%= name %>", name, header_path) {}
   };
   <% end %>
