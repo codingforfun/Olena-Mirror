7f7a65ad2b2cb35d254254db6769f3de8c2e2eba
Roland Levillain <roland@lrde.epita.fr>
2006-07-13 16:20:10 +0200

    2006-07-13  Roland Levillain  <roland@lrde.epita.fr>
    	* ruby/ltrequire.rb: Add copyright header.
    
        prcs2git-id: 10.244

--- tools/swilena/ruby/ltrequire.rb
+++ tools/swilena/ruby/ltrequire.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2006 EPITA Research and Development Laboratory.
+#
+# This file is part of the Olena Library.  This library is free
+# software; you can redistribute it and/or modify it under the terms
+# of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; see the file COPYING.  If not, write to
+# the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+# Boston, MA 02111-1307, USA.
+
 # Extension of the Kernel.require method understanding Libtool libraries.
 # Inspired by ltihooks.py.
 
