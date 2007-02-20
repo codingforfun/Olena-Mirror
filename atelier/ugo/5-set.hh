b688867ab1cadb23ab284bbb1ce0c38db82a65c7
Ugo Jardonnet <jardonnet@lrde.epita.fr>
2007-02-20 16:36:29 +0000

    2007-02-20  Ugo Jardonnet  <jardonnet@saint-tropez.lrde.epita.fr>
    	Exercises : Day 1.
    
    	*concept.hh: New.
    
    
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@762 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- atelier/ugo/5-set.hh
+++ atelier/ugo/5-set.hh
@@ -57,15 +57,47 @@ namespace ugo
 
       stc_using(point);
 
-      unsigned	impl_npoints()                { return nb;   }
-      bool	impl_includes(const point& p) { return true; }
+      unsigned	impl_npoints()                 { return nb;     }
+      bool impl_includes(const point& p) const { return false;  }
+
     private:
-      int nb;
-      T truc;
+      int	nb;
+      T		truc;
   };
 
 
 
+  //--box_------------------------
+
+  template <typename P> struct box_;
+
+  template <typename T>
+  struct vtypes< box_<T> >
+  {
+      typedef box_<T>		         current;
+      typedef point_set_base< current >  super_type;
+  };
+
+  template <typename P>
+  struct box_ : public point_set_base< box_<P> >
+  {
+      typedef box_<P>			current;
+      typedef point_set_base< current >	super;
+
+      stc_using(box);
+
+      P pmin, pmax;
+
+      enum { n = mlc_value(typename P::dim) };
+
+      const box& impl_bbox() const { return *this; }
+  };
+
+
+
+  //-----
+
+  typedef box_< point2d_<int> > box2d;
 }
 
 #endif
---
+++ atelier/ugo/concept.hh
@@ -0,0 +1,90 @@
+#ifndef   	CONCEPT_HH_
+# define   	CONCEPT_HH_
+
+//--includes---------------------
+
+#include <mlc/assert.hh>
+#include <mlc/cmp.hh>
+#include <mlc/int.hh>
+#include <cassert>
+#include "../local/scoop.hh"
+
+namespace ugo
+{
+
+  //--equipement-------------------
+
+  stc_equip_namespace;
+
+  stc_decl_associated_type( grid  );
+  stc_decl_associated_type( coord );
+  stc_decl_associated_type( dim   );
+  stc_decl_associated_type( value );
+  stc_decl_associated_type( point );
+  stc_decl_associated_type( iter  );
+
+
+
+
+  //--Point------------------------
+
+  template <typename Exact>
+  struct Point : public virtual any<Exact>,
+		 public automatic::get_impl<Point, Exact>
+  {
+      stc_typename(grid);
+      stc_typename(coord);
+      stc_typename(dim);
+
+      enum { n = mlc_value(dim) };
+
+      bool operator== (Point<Exact> const& rhs) const	{ return this->exact().impl_equal ( rhs.exact () );  }
+      bool operator< (Point<Exact> const& rhs) const	{ return this->exact().impl_inf ( rhs.exact () );    }
+      bool operator!= (Point<Exact> const& rhs) const	{ return this->exact().impl_dif ( rhs.exact () );    }
+      bool operator> (Point<Exact> const& rhs) const	{ return this->exact().impl_sup ( rhs.exact () );    }
+      bool operator>= (Point<Exact> const& rhs) const	{ return this->exact().impl_supeq ( rhs.exact () );  }
+      bool operator<= (Point<Exact> const& rhs) const	{ return this->exact().impl_infequ ( rhs.exact () ); }
+    protected:
+      Point() {}
+  };
+
+
+
+  //--Iterator---------------------
+
+  template <typename Exact>
+  struct Iterator : public any<Exact>,
+		    public automatic::get_impl<Iterator, Exact>
+  {
+      stc_typename(value);
+
+      void start()		{ this->exact().impl_start();		}
+      void next()		{ this->exact().impl_next();		}
+      void invalidate()		{ this->exact().impl_invalidate();	}
+      bool is_valid() const	{ return this->exact().impl_is_valid(); }
+      void set(const value& v)	{ this->exact().impl_set(v);		}
+  };
+
+
+  //--Point_set-------------------
+
+    template <typename Exact>
+  class Point_set : public virtual any< Exact >,
+		    public automatic::get_impl<Point_set, Exact>
+  {
+      stc_typename( point );
+      stc_typename( grid  );
+      stc_typename( iter  );
+
+      unsigned	npoints() const		         { return this->exact().impl_npoints();   }
+      bool	includes(const point& p) const   { return this->exact().impl_includes(p); }
+    protected:
+      Point_set() {}
+  };
+
+
+
+  //--
+}
+
+#endif	    /* !CONCEPT_HH_ */
