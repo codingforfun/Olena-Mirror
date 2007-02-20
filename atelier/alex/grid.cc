e87f06ca2fd3311c1ffc4ddd8aea109254ccacad
Alexandre Abraham <abraham@lrde.epita.fr>
2007-02-20 16:58:34 +0000

    box iterator working
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@763 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- atelier/alex/grid.cc
+++ atelier/alex/grid.cc
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "grid2.hh"
 
+namespace glg
+{
+  typedef point2d_<int> point2d;
+  typedef box_<point2d> box2d;
+  typedef box_iterator_<point2d> box_iterator2d;
+}
+
 int main ()
 {
-  glg::point2d_<int> p1(3, 2), p2(7,5), p3(5,4);
-  glg::box_<glg::point2d_<int> > b(p1, p2);
-  //  glg::box2d<int> bo(p1, p2);
+  using namespace glg;
+  point2d p1(3, 2), p2(7,5), p3(5,4);
+  box2d   b(p1, p2);
+  box_iterator2d bb(b);
+  //  box2d<int> bo(p1, p2);
 
   std::cout << b.includes(p3) << std::endl;
 
