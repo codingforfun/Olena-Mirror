36c087602be8bffbdae028651e1afcd58f549cff
Alexandre Abraham <abraham@lrde.epita.fr>
2007-02-20 14:51:44 +0000

    includes works!
    git-svn-id: https://svn.lrde.epita.fr/svn/oln/prototypes@757 4aad255d-cdde-0310-9447-f3009e2ae8c0

--- atelier/alex/grid.cc
+++ atelier/alex/grid.cc
@@ -3,11 +3,11 @@
 
 int main ()
 {
-  glg::point2d_<int> p1(3, 4), p2(7,2), p3(5,3);
+  glg::point2d_<int> p1(3, 2), p2(7,5), p3(5,4);
   glg::box_<glg::point2d_<int> > b(p1, p2);
   //  glg::box2d<int> bo(p1, p2);
 
-  //  std::cout << b.includes(p3) << std::endl;
+  std::cout << b.includes(p3) << std::endl;
 
   return bidon (p1, p2);
 }
