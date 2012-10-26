be9340afeac8315e35e9a92a2b264342e5e1fe1d
Guillaume Lazzara <z@lrde.epita.fr>
2012-10-26 18:06:55 +0200

    Add sum functions.
    	* mln/fun/vv2v/sum.hh,
    	* mln/fun/vvvv2v/sum.hh: New.

---
+++ milena/mln/fun/vv2v/sum.hh
@@ -0,0 +1,75 @@
+// Copyright (C) 2012 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+#ifndef MLN_FUN_VV2V_SUM_HH
+# define MLN_FUN_VV2V_SUM_HH
+
+/// \file
+///
+/// Functor that computes the sum of two values.
+
+# include <mln/core/concept/function.hh>
+
+
+namespace mln
+{
+
+  namespace fun
+  {
+
+    namespace vv2v
+    {
+
+      /// \brief A functor computing the sum of two values.
+      template <typename V, typename R = V>
+      struct sum : public Function_vv2v< sum<V,R> >,
+		   private mlc_converts_to(R,V)::check_t
+      {
+	typedef R result;
+	typedef V argument;
+	R operator()(const V& v1, const V& v2) const;
+      };
+
+
+# ifndef MLN_INCLUDE_ONLY
+
+      template <typename V, typename R>
+      inline
+      R
+      sum<V,R>::operator()(const V& v1, const V& v2) const
+      {
+	return R(v1 + v2);
+      }
+
+# endif // ! MLN_INCLUDE_ONLY
+
+    } // end of namespace mln::fun::vv2v
+
+  } // end of namespace mln::fun
+
+} // end of namespace mln
+
+
+#endif // ! MLN_FUN_VV2V_SUM_HH
---
+++ milena/mln/fun/vvvv2v/sum.hh
@@ -0,0 +1,76 @@
+// Copyright (C) 2012 EPITA Research and Development Laboratory (LRDE)
+//
+// This file is part of Olena.
+//
+// Olena is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, version 2 of the License.
+//
+// Olena is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Olena.  If not, see <http://www.gnu.org/licenses/>.
+//
+// As a special exception, you may use this file as part of a free
+// software project without restriction.  Specifically, if other files
+// instantiate templates or use macros or inline functions from this
+// file, or you compile this file and link it with other files to produce
+// an executable, this file does not by itself cause the resulting
+// executable to be covered by the GNU General Public License.  This
+// exception does not however invalidate any other reasons why the
+// executable file might be covered by the GNU General Public License.
+
+#ifndef MLN_FUN_VVVV2V_SUM_HH
+# define MLN_FUN_VVVV2V_SUM_HH
+
+/// \file
+///
+/// Functor that computes the sum of four values.
+
+# include <mln/core/concept/function.hh>
+
+
+namespace mln
+{
+
+  namespace fun
+  {
+
+    namespace vvvv2v
+    {
+
+      // FIXME: Doc.
+
+      /// \brief A functor computing the sum of two values.
+      template <typename T, typename R=T>
+      struct sum : public Function_vvvv2v< sum<T> >
+      {
+	typedef R result;
+	typedef T argument;
+	R operator()(const T& v1, const T& v2, const T& v3, const T& v4) const;
+      };
+
+
+# ifndef MLN_INCLUDE_ONLY
+
+      template <typename T, typename R>
+      inline
+      R
+      sum<T,R>::operator()(const T& v1, const T& v2, const T& v3, const T& v4) const
+      {
+	return R(v1 + v2 + v3 + v4);
+      }
+
+# endif // ! MLN_INCLUDE_ONLY
+
+    } // end of namespace mln::fun::vvvv2v
+
+  } // end of namespace mln::fun
+
+} // end of namespace mln
+
+
+#endif // ! MLN_FUN_VVVV2V_SUM_HH
