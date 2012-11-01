d821fead0f0deb1458d0f2a5cc25310df44bb1ef
Guillaume Lazzara <z@lrde.epita.fr>
2012-11-01 18:10:20 +0100

    mln/world/kn/fill_1_from_2_faces.hh: Rely on fill_1_from_aux_2_faces.

--- milena/mln/world/kn/fill_1_from_2_faces.hh
+++ milena/mln/world/kn/fill_1_from_2_faces.hh
@@ -30,11 +30,9 @@
 #ifndef MLN_WORLD_KN_FILL_1_FROM_2_FACES_HH
 # define MLN_WORLD_KN_FILL_1_FROM_2_FACES_HH
 
-# include <mln/core/alias/point2d.hh>
-# include <mln/world/kn/is_1_face_vertical.hh>
-# include <mln/world/kn/is_1_face_horizontal.hh>
-# include <mln/world/kn/border/compute_1_faces.hh>
-# include <mln/world/kn/safe_cast.hh>
+# include <mln/core/concept/image.hh>
+# include <mln/core/concept/function.hh>
+# include <mln/world/kn/fill_1_from_aux_2_faces.hh>
 
 namespace mln
 {
@@ -84,59 +82,26 @@ namespace mln
 
 
       template <typename I, typename A>
-      void fill_1_from_2_faces(Image<I>& inout_, const Accumulator<A>& accu_)
+      void fill_1_from_2_faces(Image<I>& inout, const Accumulator<A>& accu)
       {
 	trace::entering("mln::world::kn::fill_1_from_2_faces");
 
-	I& inout = exact(inout_);
-	mln_precondition(inout.is_valid());
+	mln_precondition(exact(inout).is_valid());
 
-	A accu = exact(accu_);
-	typedef mln_argument(A) arg;
-	mln_piter(I) p(inout.domain());
-	for_all(p)
-	  if (kn::is_1_face_vertical(p))
-	  {
-	    accu.init();
-	    if (inout.domain().has(p + left))
-	      accu.take(safe_cast_to<arg>(inout(p + left)));
-	    if (inout.domain().has(p + right))
-	      accu.take(safe_cast_to<arg>(inout(p + right)));
-	    inout(p) = safe_cast(accu.to_result());
-	  }
-	  else if (is_1_face_horizontal(p))
-	  {
-	    accu.init();
-	    if (inout.domain().has(p + up))
-	      accu.take(safe_cast_to<arg>(inout(p + up)));
-	    if (inout.domain().has(p + down))
-	      accu.take(safe_cast_to<arg>(inout(p + down)));
-	    inout(p) = safe_cast(accu.to_result());
-	  }
+	kn::fill_1_from_aux_2_faces(inout, inout, accu);
 
 	trace::exiting("mln::world::kn::fill_1_from_2_faces");
       }
 
 
       template <typename I, typename F>
-      void fill_1_from_2_faces(Image<I>& inout_, const Function_vv2v<F>& f_)
+      void fill_1_from_2_faces(Image<I>& inout, const Function_vv2v<F>& f)
       {
 	trace::entering("mln::world::kn::fill_1_from_2_faces");
 
-	I& inout = exact(inout_);
-	const F& f = exact(f_);
-	mln_precondition(inout.is_valid());
+	mln_precondition(exact(inout).is_valid());
 
-	kn::border::compute_1_faces(inout, f);
-
-	mln_piter(I) p(inout.domain());
-	for_all(p)
-	  if (kn::is_1_face_vertical(p))
-	    inout(p) = safe_cast(f(safe_cast(inout(p + left)),
-				   safe_cast(inout(p + right))));
-	  else if (is_1_face_horizontal(p))
-	    inout(p) = safe_cast(f(safe_cast(inout(p + up)),
-				   safe_cast(inout(p + down))));
+	kn::fill_1_from_aux_2_faces(inout, inout, f);
 
 	trace::exiting("mln::world::kn::fill_1_from_2_faces");
       }
