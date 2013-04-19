00a1e3480ea7c57d1b82d336a658127223f7c173
Guillaume Lazzara <z@lrde.epita.fr>
2013-04-19 14:25:51 +0200

    Explain how to iterate over an image in tutorial.
    	* doc/examples/split/tuto3_rw_image-3.cc.raw,
    	* doc/examples/split/tuto3_rw_image-4.cc.raw,
    	* doc/examples/split/tuto3_rw_image-5.cc.raw,
    	* doc/examples/split/tuto3_rw_image-6.cc.raw,
    	* doc/examples/split/tuto3_rw_image-7.cc.raw,
    	* doc/examples/split/tuto3_rw_image-8.cc.raw: Regen.
    
    	* doc/examples/tuto3_rw_image.cc: Add an example with iterators.
    
    	* doc/tutorial.tex: Add more explanations on iterators.

--- milena/doc/examples/tuto3_rw_image.cc
+++ milena/doc/examples/tuto3_rw_image.cc
@@ -49,6 +49,12 @@ int main()
   // \}
 
   // \{
+  mln_piter(image2d<value::rgb8>) p(ima.domain());
+  for_all(p)
+    ima(p) = literal::red;
+  // \}
+
+  // \{
   for (def::coord row = 20; row < 30; ++row)
     for (def::coord col = 20; col < 30; ++col)
       ima(point2d(row, col)) = literal::blue;
