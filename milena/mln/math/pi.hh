2a636844b5ed4d38e4e1833d15035201c9d8fb93
Guillaume Lazzara <z@lrde.epita.fr>
2013-05-03 11:14:30 +0200

    mln/math/pi.hh: Make pi more precise.

--- milena/mln/math/pi.hh
+++ milena/mln/math/pi.hh
@@ -1,4 +1,4 @@
-// Copyright (C) 2009, 2010, 2012 EPITA Research and Development
+// Copyright (C) 2009, 2010, 2012, 2013 EPITA Research and Development
 // Laboratory (LRDE)
 //
 // This file is part of Olena.
@@ -43,7 +43,7 @@ namespace mln
 
 #  ifndef MLN_WO_GLOBAL_VARS
 
-    const double pi = 3.1415926535897932385;
+    const double pi = 3.141592653589793238462;
 
 #  endif // ! MLN_WO_GLOBAL_VARS
 
